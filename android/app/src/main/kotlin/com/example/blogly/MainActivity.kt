2998a7bdaf2d0920fc94f2df6d76b5bb04c12dcb
JosephAwuku33 <33jaykay@gmail.com>
2023-06-27 14:00:01 +0000

    this be ma first commit

---
+++ android/app/src/main/kotlin/com/example/blogly/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.blogly
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,15 @@
+// ignore: file_names
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey[300],
+        body: const Center(
+          child: Text("Login Page"),
+        ));
+  }
+}
