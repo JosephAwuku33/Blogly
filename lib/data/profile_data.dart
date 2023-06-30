b3e1003b6bd1bc38417a1b51a10494517f18fb82
JosephAwuku33 <33jaykay@gmail.com>
2023-06-30 12:25:48 +0000

    edited the profile data

--- lib/data/profile_data.dart
+++ lib/data/profile_data.dart
@@ -1,5 +1,7 @@
 import 'package:blogly/models/user.dart';
 
 final List<User> userProfile = [
-  User(name: "Joseph Awuku", about: " A proud dark and handsome boy")
+  User(
+      name: "Joseph Awuku",
+      about: " The quieter you become the more you're able to hear")
 ];
