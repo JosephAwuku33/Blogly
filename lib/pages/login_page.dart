826f8c974a3374b447711c014bddc5944e4cd34b
JosephAwuku33 <33jaykay@gmail.com>
2023-08-09 09:57:06 +0000

    adding firebase and firestore

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 // ignore: file_names
+import 'package:blogly/widgets/form_widget.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
@@ -8,8 +9,17 @@ class Login extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.grey[300],
-        body: const Center(
-          child: Text("Login Page"),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Center(
+                  child: Image.network(
+                      "https://images.unsplash.com/photo-1633410189542-36d96e3762b8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80")),
+              CustomForm(
+                key: key,
+              )
+            ],
+          ),
         ));
   }
 }
